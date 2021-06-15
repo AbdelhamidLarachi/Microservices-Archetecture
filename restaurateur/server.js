7de2fd5c30597a894efb22d9250b5354152f4cad
nginx-iwnl <abdelhamid.larachi.dz@viacesi.fr>
2021-06-15 20:11:43 +0100

    updated

--- restaurateur/server.js
+++ restaurateur/server.js
@@ -1,8 +1,33 @@
-const app = require("./src/app");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./src/config/default.json');
+const bodyParser = require('body-parser');
+var cors = require('cors');
+const app = express();
 
-mongoose.connect(process.env.MONGO_DB_URI);
 
-app.listen(process.env.PORT, () => {
-  console.log(`running on port ${process.env.PORT}`);
-});
+app.use(cors())
+// Bodyparser Middleware
+app.use(bodyParser.json())
+app.use(express.json());
+
+// DB Config
+const db = config.mongoURI;
+
+// Connect to MongoDB
+mongoose.set('useUnifiedTopology', true);
+mongoose
+	.connect(db, {
+		useNewUrlParser: true,
+		useCreateIndex: true
+	})
+	.then(() => console.log('MongoDB Connected'))
+	.catch((err) => console.log(err));
+// Use Routes
+app.use('/api/articles', require('./src/routes/articles'));
+
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
