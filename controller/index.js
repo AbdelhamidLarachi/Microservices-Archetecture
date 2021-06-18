3ac5ab7a34b930df621d5da8566eb7a5cb53a0e0
nginx-iwnl <abdelhamid.larachi.dz@viacesi.fr>
2021-06-18 12:13:05 +0100

    execution controller update

--- controller/index.js
+++ controller/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const ip = require("ip");
-const fetch = require('node-fetch');
 const local_ip = ip.address();
+const cors = require('cors')
+const proxy = require('express-http-proxy');
 
 
 /* A LOAD BALANCED EXECUTION CONTROLLER 
@@ -11,8 +12,11 @@ const local_ip = ip.address();
 - RETURN RESULT */
 
 
-app.use(express.json()); // for 
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
 
 
 
@@ -23,10 +27,24 @@ REDIRECT request to reverse proxy
 */
 
 
+
 app.all('*', async function (req, res, next) {
 
 	try 
 	{
+		// Unsupported AppName
+		const appName = req.url.split("/")[1];
+
+		/* 
+		- CHECK Server STATUS
+		- HANDLE Environement VARIABLES
+		*/
+
+		if(appName == 'status') {
+			res.json({BUILD_V : process.env.BUILD_V, REVERSE_PROXY_URL : process.env.REVERSE_PROXY_URL, APPID: process.env.APPID});
+		}
+
+
 		// No Version
 		if(req.body.version == undefined) 
 		{
@@ -42,46 +60,24 @@ app.all('*', async function (req, res, next) {
 		}
 
 
-		// Unsupported AppName
-		const appName = req.url.split("/")[1];
-
 		if(!['client', 'restaurateur', 'livreur', 'commercial', 'technique'].includes(appName))
 		{
 			res.status(505).json({ error: "Unsupported app-name." });
 			return;
 		}
 
-
-		// Handle request to reverse proxy..
-
-        const options = {
-            method: req.method,
-            headers: req.headers,
-            body: req.body
-        }
-
-		let response = await fetch(`http://${process.env.LOCAL_IP}:${process.env.REVERSE_PROXY_PORT}${req.url}`, options);
-        let result = await response.json();
-        res.json(result);
+		next();
     }
 
     catch(error) 
     {
-        res.status(500).json(error);
+        res.status(500).send(error);
     }
 
-    next();
-})
+}, proxy(process.env.REVERSE_PROXY_URL))
 
 
-/* 
-- CHECK Server STATUS
-- HANDLE Environement VARIABLES
-*/
 
-app.get("/status", async (req, res) => {
-    res.json({BUILD_V : process.env.BUILD_V, REVERSE_PROXY_PORT : process.env.REVERSE_PROXY_PORT, APPID: process.env.APPID});
-});
 
 
 app.listen(process.env.APPID, () => console.log(`server is listening on ${process.env.APPID}`))
