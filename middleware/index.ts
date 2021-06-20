661e3a724adc0cd2f5800cd59720fe146489ca18
nginx-iwnl <abdelhamid.larachi.dz@viacesi.fr>
2021-06-20 16:55:52 +0100

    middleware update

--- middleware/index.ts
+++ middleware/index.ts
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const sql = require("mssql");
 const bcrypt = require('bcrypt');
 const proxy = require('express-http-proxy');
+const ip = require("ip");
+
+import { NextFunction, Request, Response } from 'express';
 require('dotenv/config');
 
 
@@ -23,12 +26,64 @@ app.use(morgan('dev')); /* HTTP request logger middleware */
 const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  server: process.env.LOCAL_IP, 
+  server: ip.address(), 
   database: process.env.DB_NAME,
   options: {
     // True for local dev / self-signed certification
     trustServerCertificate: true
   }
+}
+
+
+
+// @type role
+enum role {
+  client,
+  restaurateur,
+  livreur,
+  technique,
+  commercial
+}
+
+
+// @Auth form 
+interface IAuth {
+  //timestamps?: Date;
+  email: string;
+  password: string;
+  role: number;
+  version?: number;
+}
+
+
+
+// server-sided email pattern check
+function isValidEmail(email: string) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+
+// server-sided password pattern check
+function isValidPassword(password: string) {
+
+  /*
+  (?=.*\d)          // should contain at least one digit
+  (?=.*[a-z])       // should contain at least one lower case
+  (?=.*[A-Z])       // should contain at least one upper case
+  [a-zA-Z0-9]{8,}   // should contain at least 8 from the mentioned characters
+  */
+
+  const re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+  return re.test(String(password));
+}
+
+
+
+// @type assertion as IAuth
+function isValidForm(user: any): user is IAuth {
+  return (isValidEmail(user.email) &&
+  (isValidPassword(user.password)) && (typeof (user.version) == "number"));
 }
 
 
@@ -42,7 +97,8 @@ const config = {
   */
 
 
-  app.all('*', (req, res, next) => {  
+
+  app.all('*', (req: Request, res: Response, next: NextFunction) => {  
     
     if(req.url.includes('/login'))
       return login(req, res, next);
@@ -64,7 +120,7 @@ const config = {
 
 
   
-  function verifyJWT(req, res, next) {
+  function verifyJWT(req: any, res: Response, next: NextFunction) {
 
     if(req.url.includes('/user')) 
       return next();
@@ -75,7 +131,7 @@ const config = {
       req.token = bearerHeader.split(' ')[1];
 
       // jwt auth
-      jwt.verify(req.token, process.env.SECRET_KEY, (err, auth) => {
+      jwt.verify(req.token, process.env.SECRET_KEY, (err: Error, auth?: any) => {
       
       if(err)
         res.sendStatus(401);
@@ -98,23 +154,34 @@ const config = {
   */
 
 
-  function login(req, res, next) {
+
+  function login(req: any, res: Response, next: NextFunction) {
+
+
+    // is undefined
+    if(!((typeof (req.body.password) == "string") && (typeof (req.body.email) == "string")))
+      res.status(401).json({err: "check inputs"});
+
 
     // connect to your database
-    sql.connect(config, function (err) {
-    
+    sql.connect(config, function (err: Error) {
+
       if (err) console.log(err);
+
 
       // create Request object
       var request = new sql.Request();
       request.input('email', req.body.email);
       
+
       // query to the database and get the records
       request.query("SELECT * FROM usr WHERE email = @email",
       
-      function(err, result){
-        if (err) console.log(err)
 
+      function(err: Error, result: any){
+        if (err) res.status(500).send()
+
+        
         // exists
         if(result.rowsAffected[0] > 0) 
         {
@@ -125,9 +192,9 @@ const config = {
 
             delete user.password;
 
-            // return token 
-            jwt.sign({ user }, process.env.SECRET_KEY, { expiresIn: '24h' }, (err, token) => {
-              res.status(200).json({ user : user, token});
+            // return token & user
+            jwt.sign({ user }, process.env.SECRET_KEY, { expiresIn: '24h' }, (err: Error, token: JsonWebKey) => {
+              res.status(200).json({ user, token});
             });
           }
 
@@ -149,31 +216,51 @@ const config = {
 
 
 
-  function signup(req, res, next) {
+  function signup(req: Request<{}, {}, IAuth>, res: Response, next: NextFunction) {
 
   // connect to your database
-  sql.connect(config, function (err) {
+  sql.connect(config, function (err?: Error) {
   
     if (err) res.status(500).send();
+
+    if(isValidForm(req.body)){
 
       // create Request object
       var request = new sql.Request();
 
+      // convert role string to its enum index
+      const user_role : string = role[req.body.role];
+
+      // fill req inputs
       request.input('email', req.body.email);
       request.input('hash', bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10)));
-      request.input('role', req.body.role);
+      request.input('role', user_role);
 
       // query to the database and get the records
       request.query("INSERT INTO usr(email, password, role) VALUES (@email, @hash, @role)",
 
-      function(err, result){
+      // internal error
+      function(err?: Error){
         if (err) res.status(500).send();
         
         // inserted
         res.status(201).send();
       });
+    }
+
+
+    // bad request 401
+    else {
+      res.status(401).json({
+        expects: {
+          email: "string (required)",
+          password: "string (required) at least one digit, one lower case, one upper case, 8 characters",
+          role: Object.values(role).filter(key => isNaN(Number(key))) // iterate role enum props
+        }
+      });
+    }
   });
-  }
+}
 
 
 
