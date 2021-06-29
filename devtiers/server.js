7645eeb7a9c6fc10f0db199f2998657c3089c87d
Klimqx <klimqxcs@gmail.com>
2021-06-29 19:51:15 +0100

    initialized devtiers

---
+++ devtiers/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./src/config/default.json');
+const bodyParser = require('body-parser');
+var cors = require('cors');
+const app = express();
+
+
+app.use(cors())
+// Bodyparser Middleware
+app.use(bodyParser.json())
+app.use(express.json());
+
+// DB Config
+const db = config.mongoURI;
+
+// Connect to MongoDB
+mongoose.set('useUnifiedTopology', true);
+mongoose
+	.connect(db, {
+		useNewUrlParser: true,
+		useCreateIndex: true
+	})
+	.then(() => console.log('MongoDB Connected'))
+	.catch((err) => console.log(err));
+// Use Routes
+app.use('/api/articles', require('./src/routes/articles'));
+
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
