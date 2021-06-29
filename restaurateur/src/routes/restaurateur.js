192f4892dae616c4a33b51e3f76bdc549ddd037f
Klimqx <klimqxcs@gmail.com>
2021-06-29 15:20:13 +0100

    updated restaurateur.js

--- restaurateur/src/routes/restaurateur.js
+++ restaurateur/src/routes/restaurateur.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 // Restaurateur Model
 const Restaurateur = require("../../src/models/Restaurateur");
-const Articles = require("../../src/models/Articles");
 
 // @route GET api/Restaurateurs
 // @desc  GET ALL Restaurateurs
